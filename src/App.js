c587c6b193f5cd096bacc36e6e5bc79be8923299
Volkov Danil <snake1234567890-1@yandex.ru>
2023-01-10 18:32:35 +0300

    feat: add file birth_f and make output

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [parts, setParts] = useState([]);
+  const [congratulation, setCongratulation] = useState('');
+
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+  useEffect(() => {
+    fetch('/birth_f.json')
+    .then(res => res.json())
+    .then(res => {
+      const result = [];
+      for (const key in res) {
+        if (Object.hasOwnProperty.call(res, key)) {
+          result.push([[key], res[key][randomIntFromInterval(0, res[key].length - 1)]]);          
+        }
+      }
+      setParts(result);
+    })
+    .catch(err => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    const str = parts.map((part) => `${part[0]} ${part[1]}`).join(' ');
+    setCongratulation(str);
+  }, [parts]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {parts.map((part) => <li>{part[0]} {part[1]}</li>)}
+      </ul>
+      <h2>{congratulation}</h2>
     </div>
   );
 }

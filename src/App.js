5279ba046f50974a9d13f0bf65d4668f50f2096f
Volkov Danil <snake1234567890-1@yandex.ru>
2023-01-11 12:39:01 +0300

    feat: add new_year.json and make change individual lines

--- src/App.js
+++ src/App.js
@@ -2,27 +2,34 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [columns, setColumns] = useState([]);
   const [parts, setParts] = useState([]);
   const [congratulation, setCongratulation] = useState('');
 
-  function randomIntFromInterval(min, max) {
+  const randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
-  
+
+  const changeParts = (column) => {
+    const newParts = [...parts];
+    newParts[column][1] = columns[column][randomIntFromInterval(1, columns[column].length - 1)];
+    setParts(newParts);
+  }
+
   useEffect(() => {
     fetch('/birth_f.json')
     .then(res => res.json())
     .then(res => {
-      const result = [];
-      for (const key in res) {
-        if (Object.hasOwnProperty.call(res, key)) {
-          result.push([[key], res[key][randomIntFromInterval(0, res[key].length - 1)]]);          
-        }
-      }
-      setParts(result);
+      setColumns(res);
     })
     .catch(err => console.error(err));
   }, []);
+
+  useEffect(() => {
+      const result = [];
+      columns.map(column => result.push([column[0], column[randomIntFromInterval(1, column.length - 1)]]))
+      setParts(result);
+  }, [columns]);
 
   useEffect(() => {
     const str = parts.map((part) => `${part[0]} ${part[1]}`).join(' ');
@@ -31,7 +38,7 @@ function App() {
   return (
     <div className="App">
       <ul>
-        {parts.map((part) => <li>{part[0]} {part[1]}</li>)}
+        {parts.map((part, index) => <li key={part[0]}>{part[0]} {part[1]} <button onClick={() => changeParts(index)}>поменять</button></li>)}
       </ul>
       <h2>{congratulation}</h2>
     </div>

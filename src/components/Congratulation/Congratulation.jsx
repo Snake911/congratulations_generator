b2730a0812884a0b9df5863230b4b777b07348e8
Volkov Danil <snake1234567890-1@yandex.ru>
2023-01-31 14:23:51 +0300

    fix: change Clipboard to async

--- src/components/Congratulation/Congratulation.jsx
+++ src/components/Congratulation/Congratulation.jsx
@@ -2,18 +2,22 @@ import './Congratulation.css';
 
 export const Congratulation = ({ congratulation }) => {
   const copyText = (e) => {
-    const type = "text/plain";
-    const blob = new Blob([congratulation], { type });
-    const data = [new ClipboardItem({ [type]: blob })];
-    navigator.clipboard.write(data)
-    .then(() => {
-      console.log(e.target.parentElement.classList.add('active'));
-      setTimeout(() => {
-        e.target.parentElement.classList.remove('active')
-      }, 2000);
-    }, (err) => {
-      console.log(err)
-    });   
+    async function copy() {
+      try {
+        const type = "text/plain";
+        const blob = new Blob([congratulation], { type });
+        await navigator.clipboard.write([
+          new ClipboardItem({ [type]: blob }),
+        ]);
+        e.target.parentElement.classList.add('active')
+        setTimeout(() => {
+            e.target.parentElement.classList.remove('active')
+          }, 2000);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    copy();
   }
   return (
   <div className='congratulationContainer' title="Скопировать" onClick={(e) => copyText(e)}>

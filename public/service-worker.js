29aad1e287c009c92bf46981a048aa0c2bdbd415
Volkov Danil <snake1234567890-1@yandex.ru>
2023-02-05 13:29:32 +0300

    feat: make service worker

---
+++ public/service-worker.js
@@ -0,0 +1,84 @@
+// Set this to true for production
+var doCache = true;
+
+// Name our cache
+var CACHE_NAME = 'generator';
+
+// Delete old caches that are not our current one!
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys()
+      .then(keyList =>
+        Promise.all(keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            console.log('Deleting cache: ' + key)
+            return caches.delete(key);
+          }
+        }))
+      )
+  );
+});
+
+// The first time the user starts up the PWA, 'install' is triggered.
+self.addEventListener('install', function(event) {
+  if (doCache) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          // Get the assets manifest so we can see what our js file is named
+          // This is because webpack hashes it
+          
+          fetch("asset-manifest.json")
+            .then(response => {
+              return response.json();
+            })
+            .then(assets => {
+              // Open a cache and cache our files
+              // We could also cache any static assets like CSS or images
+              const files = assets.files;
+              const cachedFiles = [];
+              for (const url in files) {
+                if (Object.hasOwnProperty.call(files, url)) {
+                  cachedFiles.push(files[url]);                  
+                }
+              }
+
+              cache.addAll(cachedFiles)
+              console.log('cached');
+            })
+            .catch((e) => {
+              console.error(e)
+            });
+
+            fetch("/settings/data.json")
+            .then(response => {
+              return response.json();
+            })
+            .then(settings => {
+              const cachedFiles = [];
+              settings.map((setting) => {
+                cachedFiles.push(setting.icon);
+                cachedFiles.push(`/settings/${setting.code}.json`);
+              });
+              cache.addAll(cachedFiles);
+            })
+            .catch((e) => {
+              console.error(e)
+            });
+        })
+    );
+  }
+});
+
+// When the webpage goes to fetch files, we intercept that request and serve up the matching files
+// if we have them
+self.addEventListener('fetch', function(event) {
+    if (doCache) {
+      event.respondWith(
+          caches.match(event.request).then(function(response) {
+              return response || fetch(event.request);
+          })
+      );
+    }
+});
